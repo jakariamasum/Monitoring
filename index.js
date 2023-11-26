b196eb1c73249e96913a7820cb80e9695d319f9b
jakariamasum <jmphy698@gmail.com>
2023-11-26 23:53:19 +0600

    refractor

--- index.js
+++ index.js
@@ -1,46 +1,24 @@
-//dependencies 
 const http = require('http');
-const url = require('url')
-const { StringDecoder } = require('string_decoder')
+const { handleReqRes } = require('./helpers/handleReqRes');
+
 // app object - module scaffolding
 const app = {};
 
-
-//configuration
+// configuration
 app.config = {
     port: 3000,
-}
+};
 
-
-//create server
+// create server
 app.createServer = () => {
-    const server = http.createServer(app.handleReqRes)
+    const server = http.createServer(app.handleReqRes);
     server.listen(app.config.port, () => {
-        console.log('Server running')
-    })
-}
-
-app.handleReqRes = (req, res) => {
-    const parseUrl = url.parse(req.url, true)//returns objects about url
-    // console.log(parseUrl)
-    const path = parseUrl.pathname;
-    const trimedPath = path.replace(/^\/+|\/+$/g, '')//replace the /  with '' i.e /about/next --> about/next 
-    const method = req.method.toLowerCase()
-    const queryStringObject = parseUrl.query;
-    const headerObject = req.headers;
-
-    const decoder = new StringDecoder('utf-8');
-    let realData = '';
-
-    req.on('data', (buffer) => {
-        realData += decoder.write(buffer);
+        console.log(`listening to port ${app.config.port}`);
     });
+};
 
-    req.on('end', () => {
-        realData += decoder.end()
-        console.log(realData)
-        res.end('Hello how are you!')
-    })
-}
+// handle Request Response
+app.handleReqRes = handleReqRes;
 
-app.createServer()
+// start the server
+app.createServer();
