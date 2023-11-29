5e1b8f864bd7c57ee659078e3ce299ab2209a932
jakariamasum <jmphy698@gmail.com>
2023-11-29 14:46:17 +0600

    response header add

--- helpers/handleReqRes.js
+++ helpers/handleReqRes.js
@@ -32,17 +32,6 @@ handler.handleReqRes = (req, res) => {
     console.log(routes[trimmedPath])
     const chosenHandler = routes[trimmedPath] ? routes[trimmedPath] : notFoundHandler;
 
-    chosenHandler(requestProperties, (statusCode, payload) => {
-        statusCode = typeof statusCode === 'number' ? statusCode : 500;
-        payload = typeof payload === 'object' ? payload : {};
-
-        const payloadString = JSON.stringify(payload);
-
-        // return the final response
-        res.writeHead(statusCode);
-        res.end(payloadString);
-    });
-
     req.on('data', (buffer) => {
         realData += decoder.write(buffer);
     });
@@ -50,10 +39,19 @@ handler.handleReqRes = (req, res) => {
     req.on('end', () => {
         realData += decoder.end();
 
-        console.log(realData);
-        // response handle
-        res.end('Hello world');
+        chosenHandler(requestProperties, (statusCode, payload) => {
+            statusCode = typeof statusCode === 'number' ? statusCode : 500;
+            payload = typeof payload === 'object' ? payload : {};
+
+            const payloadString = JSON.stringify(payload);
+
+            // return the final response
+            res.setHeader('Content-Type','application/json')
+            res.writeHead(statusCode);
+            res.end(payloadString);
+        });
     });
+
 };
 
 module.exports = handler;
