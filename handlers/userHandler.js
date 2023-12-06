2d9d2629a4c20269f3c5beeca5d8cb59fc58d794
jakariamasum <jmphy698@gmail.com>
2023-12-06 22:44:00 +0600

    file upate

--- handlers/userHandler.js
+++ handlers/userHandler.js
@@ -102,7 +102,91 @@ handler._user.post = (requestProperties, callback) => {
         });
     }
 };
-handler._user.put = (requestProperties, callback) => { };
+handler._user.put = (requestProperties, callback) => {
+    const phone =
+        typeof requestProperties.body.phone === 'string' &&
+            requestProperties.body.phone.trim().length === 11
+            ? requestProperties.body.phone
+            : false;
+
+    const firstName =
+        typeof requestProperties.body.firstName === 'string' &&
+            requestProperties.body.firstName.trim().length > 0
+            ? requestProperties.body.firstName
+            : false;
+
+    const lastName =
+        typeof requestProperties.body.lastName === 'string' &&
+            requestProperties.body.lastName.trim().length > 0
+            ? requestProperties.body.lastName
+            : false;
+
+    const password =
+        typeof requestProperties.body.password === 'string' &&
+            requestProperties.body.password.trim().length > 0
+            ? requestProperties.body.password
+            : false;
+
+    if (phone) {
+        if (firstName || lastName || password) {
+            // verify token
+            const token =
+                typeof requestProperties.headersObject.token === 'string'
+                    ? requestProperties.headersObject.token
+                    : false;
+
+            tokenHandler._token.verify(token, phone, (tokenId) => {
+                if (tokenId) {
+                    // loopkup the user
+                    data.read('users', phone, (err1, uData) => {
+                        const userData = { ...parseJSON(uData) };
+
+                        if (!err1 && userData) {
+                            if (firstName) {
+                                userData.firstName = firstName;
+                            }
+                            if (lastName) {
+                                userData.firstName = firstName;
+                            }
+                            if (password) {
+                                userData.password = hash(password);
+                            }
+
+                            // store to database
+                            data.update('users', phone, userData, (err2) => {
+                                if (!err2) {
+                                    callback(200, {
+                                        message: 'User was updated successfully!',
+                                    });
+                                } else {
+                                    callback(500, {
+                                        error: 'There was a problem in the server side!',
+                                    });
+                                }
+                            });
+                        } else {
+                            callback(400, {
+                                error: 'You have a problem in your request!',
+                            });
+                        }
+                    });
+                } else {
+                    callback(403, {
+                        error: 'Authentication failure!',
+                    });
+                }
+            });
+        } else {
+            callback(400, {
+                error: 'You have a problem in your request!',
+            });
+        }
+    } else {
+        callback(400, {
+            error: 'Invalid phone number. Please try again!',
+        });
+    }
+};
 handler._user.delete = (requestProperties, callback) => {
     const phone =
         typeof requestProperties.queryStringObject.phone === 'string' &&
